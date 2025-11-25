bb73f7b4bc4841b6af175d8459a0f9828be4018c
1280042 <2400821h@gsuite.kobe-u.ac.jp>
2025-11-25 23:30:34 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,46 +1,35 @@
 // 最終課題を制作しよう
 let x, y;
-let vx = 0
-let vy = 0
 let w, h
-
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  background(160, 192, 255);
+  background(255);
   x = width / 2;
   y = height / 2;
-  let widths = []
-  let heights = []
-  
-    // line(x, y, random(0, width), random(0, height));
-  
 }
-
 
 function draw(){
-  background(160, 192, 255);
-    // widths[i] = random(0, width) 
-    // heights[i] = random(0, height)
- 
-  // for(let i = 0; i < 10; i++){
-  // widths[i] = random(0, width);
-  // heights[i] =  random(0, height);
-  
-  // w = widths[1]
-  // h = heights[1]
-  // }
-  line(x, y, random(0,width), random(0,height))
-  vx = constrain(vx, 0, w)
-  vy = constrain(vy, 0, h)
-  if(vx < w){
-    vx += 1
-  }
-  if(vy < h){
-    vy += 1
-  }
+  textSize(32); 
+  textAlign(CENTER, CENTER); 
+  text("improve comunity", width / 2, height / 2); 
+  stroke(random(0, 255), random(0, 255), random(0, 255))
 }
 
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
+  for(let j = 0; j < 10; j += 1){
+   w = random(0, width)
+   h = random(0, height)
+   w1 = random(0, width)
+   h1 = random(0, height)
+   stroke(random(0, 255), random(0, 255), random(0, 255))
+   line(x, y, w, h)
+   line(w, h, w1, h1)
+   line(w1, h1, x, y)
+  }
+}
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
